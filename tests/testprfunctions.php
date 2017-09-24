cf4c43505f1a5963188829d86221e180b13e9393
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-09-24 02:18:51 +0100

    written more tests

--- tests/testprfunctions.php
+++ tests/testprfunctions.php
@@ -42,19 +42,81 @@ class StackTest extends TestCase
         calculateLength($numPosts, $numRailings);
     }
 
-    public function testSisValidFence() {
-        
+    /**
+     * testS1isValidFence uses a regular valid fence.
+     */
+    public function testS1isValidFence() {
+        $numPosts = 4;
+        $numRailings = 3;
+        $expected = true;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
     }
 
+    /**
+     * testS2isValidFence uses the minimum fence.
+     */
+    public function testS2isValidFence() {
+        $numPosts = 2;
+        $numRailings = 1;
+        $expected = true;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
+    }
+
+    /**
+     * testS3isValidFence uses too few posts.
+     */
+    public function testS3isValidFence() {
+        $numPosts = 1;
+        $numRailings = 1;
+        $expected = false;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
+    }
+
+    /**
+     * testS4isValidFence uses too few railings.
+     */
+    public function testS4isValidFence() {
+        $numPosts = 4;
+        $numRailings = 0;
+        $expected = false;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
+    }
+
+    /**
+     * testS5isValidFence the wrong ratio of posts and railings.
+     */
+    public function testS5isValidFence() {
+        $numPosts = 4;
+        $numRailings = 2;
+        $expected = false;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
+    }
+
+    /**
+     * testFisValidFence uses a negative number for the number of posts.
+     */
     public function testFisValidFence() {
-
+        $numPosts = -3;
+        $numRailings = 4;
+        $expected = false;
+        $actual = isValidFence($numPosts, $numRailings);
+        $this -> assertEquals($expected, $actual);
     }
 
+    /**
+     * testMisValidFence uses wrong data types for posts and railings.
+     */
     public function testMisValidFence() {
-
+        $numPosts = 'lemon';
+        $numRailings = 6.7;
+        $this->expectException(TypeError::class);
+        isValidFence($numPosts, $numRailings);
     }
-
-
 
 
 }
